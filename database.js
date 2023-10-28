e1a0a6429b5e2f6317c2dba9932e043ba7f60a09
Merebe <merebe2012@gmail.com>
2023-10-28 12:45:16 +0200

    Ajout des queries add,update,delete,get et getAll pour toutes les tables

--- database.js
+++ database.js
@@ -2,8 +2,7 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
-//Creating the connection to the database
-
+// Création de la connexion à la base de données
 const pool = mysql
   .createPool({
     host: process.env.HOST,
@@ -14,8 +13,9 @@ const pool = mysql
   })
   .promise();
 
-//First query
-//int verifyLogin(email,password) => return 0 if doesnt exist else return userId
+/************************************** QUERIES DES UTILISATEURS **********************************************/
+
+// Vérifier la connexion (email, mot de passe) => retourne 0 s'il n'existe pas, sinon retourne l'ID de l'utilisateur
 async function verifyLogin(email, password) {
   const [rows] = await pool.query(
     "SELECT * FROM user WHERE email=? and password=?",
@@ -23,7 +23,8 @@ async function verifyLogin(email, password) {
   );
   return rows.length === 0 ? 0 : rows[0].ID;
 }
-//bool addUser(userObject)
+
+// Ajouter un utilisateur
 async function addUser(userObject) {
   const [rows] = await pool.query(
     "INSERT INTO user (firstName,lastName,email,phone,password,role,pictureUrl,ID_restaurant) VALUES(?,?,?,?,?,?,?,?) ",
@@ -41,35 +42,105 @@ async function addUser(userObject) {
   const result = await getUser(userObject[0].ID);
   return result;
 }
-//modifyUser(userObject)
-//userObject getUser(userId) => if null user dont exist
+
+// Mettre à jour un utilisateur
+async function updateUser(userObject) {
+  const [rows] = await pool.query(
+    "UPDATE user SET firstName = ?, lastName = ?, email = ?, phone = ?, password = ?, role = ?, pictureUrl = ?, ID_restaurant = ? WHERE ID = ?",
+    [
+      userObject[0].firstName,
+      userObject[0].lastName,
+      userObject[0].email,
+      userObject[0].phone,
+      userObject[0].password,
+      userObject[0].role,
+      userObject[0].pictureUrl,
+      userObject[0].ID_restaurant,
+      userObject[0].ID,
+    ]
+  );
+}
+
+// Obtenir un utilisateur par ID, retourne 0 s'il n'existe pas
 async function getUser(id) {
   const [rows] = await pool.query("SELECT * FROM user WHERE id=?", [id]);
   return rows.length === 0 ? 0 : rows;
 }
 
-//bool addStaff(staffObject)
+// Obtenir tous les utilisateurs
+async function getAllUser() {
+  const [rows] = await pool.query("SELECT * FROM user");
+  return rows.length === 0 ? 0 : rows;
+}
+
+// Vérifier l'existence d'un utilisateur par email, retourne 0 s'il n'existe pas, sinon retourne l'ID de l'utilisateur
+async function verifyUserEmail(email) {
+  const [rows] = await pool.query("SELECT * FROM user WHERE email=?", [email]);
+  return rows.length === 0 ? 0 : rows[0].ID;
+}
+
+// Supprimer un utilisateur
+async function deleteUser(id) {
+  const user = await getUser(id);
+
+  if (user === 0) {
+    return "L'utilisateur n'existe pas.";
+  }
+
+  const [rows] = await pool.query("DELETE FROM user WHERE ID = ?", [id]);
+  return "Utilisateur supprimé avec succès.";
+}
+
+/************************************** QUERIES DU PERSONNEL **********************************************/
+
+// Ajouter un membre du personnel
 async function addStaff(staffObject) {
   const [rows] = await pool.query(
     "INSERT INTO staff (role,stars,ID_user) VALUES(?,?,?) ",
     [staffObject[0].role, staffObject[0].stars, staffObject[0].ID_users]
   );
-  const result = await getUser(staffObject[0].ID);
-  return result;
 }
-//bool modifyStaff(staffObject)
-//bool deleteStaff(staffId) => Verify if exist then delete else error message
-//int verifyUserEmail(userEmail) => return 0 if doesnt exist else return userId
-async function verifyUserEmail(email) {
-  const [rows] = await pool.query("SELECT * FROM user WHERE email=?", [email]);
-  return rows.length === 0 ? 0 : rows[0].ID;
+
+// Mettre à jour un membre du personnel
+async function updateStaff(staffObject) {
+  const [rows] = await pool.query(
+    "UPDATE user SET role = ?, stars = ?, ID_user = ? WHERE ID = ?",
+    [
+      staffObject[0].role,
+      staffObject[0].stars,
+      staffObject[0].ID_user,
+      staffObject[0].ID,
+    ]
+  );
 }
-//list of Staff getAllStaff()
+
+// Obtenir un staff par ID, retourne 0 s'il n'existe pas
+async function getStaff(id) {
+  const [rows] = await pool.query("SELECT * FROM staff WHERE id=?", [id]);
+  return rows.length === 0 ? 0 : rows;
+}
+
+// Supprimer un membre du personnel
+async function deleteStaff(id) {
+  const user = await getStaff(id);
+
+  if (staff === 0) {
+    return "Aucun staff n'existe pour cet ID.";
+  }
+
+  const [rows] = await pool.query("DELETE FROM staff WHERE ID = ?", [id]);
+  return "Staff supprimé avec succès.";
+}
+
+// Obtenir tous les membres du personnel
 async function getAllStaff() {
   const [rows] = await pool.query("SELECT * FROM staff");
-  return rows;
+  return rows.length === 0 ? 0 : rows;
 }
-//bool addRestaurant(restaurant object)
+
+/************************************** QUERIES DES RESTAURANTS **********************************************/
+
+// Ajouter un restaurant
 async function addRestaurant(restaurantObject) {
   const [rows] = await pool.query(
     "INSERT INTO restaurant (name,adress,phone) VALUES(?,?,?) ",
@@ -79,16 +150,79 @@ async function addRestaurant(restaurantObject) {
       restaurantObject[0].phone,
     ]
   );
-  const result = await getUser(restaurantObject[0].ID);
-  return result;
 }
-//bool modifyRestaurant(restaurant object)
 
-const result = await addRestaurant([
-  {
-    name: "o",
-    adress: "o",
-    phone: "o",
-  },
-]);
-console.log(result);
+// Mettre à jour un restaurant
+async function updateRestaurant(restaurantObject) {
+  const [rows] = await pool.query(
+    "UPDATE restaurant SET name = ?, adress = ?, phone = ? WHERE ID = ?",
+    [
+      restaurantObject[0].name,
+      restaurantObject[0].adress,
+      restaurantObject[0].phone,
+      restaurantObject[0].ID,
+    ]
+  );
+}
+
+// Obtenir tous les restaurants
+async function getAllRestaurant() {
+  const [rows] = await pool.query("SELECT * FROM restaurant");
+  return rows.length === 0 ? 0 : rows;
+}
+
+// Obtenir un restaurant par ID, retourne 0 s'il n'existe pas
+async function getRestaurant(id) {
+  const [rows] = await pool.query("SELECT * FROM restaurant WHERE id=?", [id]);
+  return rows.length === 0 ? 0 : rows;
+}
+
+// Supprimer un restaurant
+async function deleteRestaurant(id) {
+  const restaurant = await getRestaurant(id);
+
+  if (restaurant === 0) {
+    return "Aucun restaurant n'existe pour cet ID.";
+  }
+
+  const [rows] = await pool.query("DELETE FROM restaurant WHERE ID = ?", [id]);
+  return "Restaurant supprimé avec succès.";
+}
+
+/************************************** QUERIES DES COMMENTAIRES **********************************************/
+
+// Ajouter un commentaire
+async function addComment(commentObject) {
+  const [rows] = await pool.query(
+    "INSERT INTO comment (firstName,lastName,ID_restaurant) VALUES(?,?,?) ",
+    [
+      commentObject[0].firstName,
+      commentObject[0].lastName,
+      commentObject[0].ID_restaurant,
+    ]
+  );
+}
+
+// Obtenir un commentaire par ID, retourne 0 s'il n'existe pas
+async function getComment(id) {
+  const [rows] = await pool.query("SELECT * FROM comment WHERE id=?", [id]);
+  return rows.length === 0 ? 0 : rows;
+}
+
+// Supprimer un commentaire
+async function deleteComment(id) {
+  const comment = await getComment(id);
+
+  if (comment === 0) {
+    return "Aucun commentaire n'existe pour cet ID.";
+  }
+
+  const [rows] = await pool.query("DELETE FROM comment WHERE ID = ?", [id]);
+  return "Commentaire supprimé avec succès.";
+}
+
+// Obtenir tous les commentaires
+async function getAllComment() {
+  const [rows] = await pool.query("SELECT * FROM comment");
+  return rows.length === 0 ? 0 : rows;
+}
