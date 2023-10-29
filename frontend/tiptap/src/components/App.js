6e9679dd6bd15672aebd06b84d2120e3299c905a
Merebe <merebe2012@gmail.com>
2023-10-29 20:11:14 +0100

    Creating home page

---
+++ frontend/tiptap/src/components/App.js
@@ -0,0 +1,12 @@
+import Container from "react-bootstrap/Container";
+import HomePage from "./HomePage";
+
+function App() {
+  return (
+    <Container>
+      <HomePage></HomePage>
+    </Container>
+  );
+}
+
+export default App;
