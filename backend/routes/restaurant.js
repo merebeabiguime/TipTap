2ca9becaa0ba215264133c0b73b152f2097e0531
Merebe <merebe2012@gmail.com>
2023-10-29 14:38:37 +0100

    Réorganisation des dossiers et fichiers pour que tout soit plus claire

---
+++ backend/routes/restaurant.js
@@ -0,0 +1,34 @@
+import express from "express";
+import * as db from "./database.js";
+const router = express.Router();
+
+/***************************************** GET ALL ***************************************************** */
+
+router.get("/", async (req, res) => {
+  const result = await db.getAllRestaurant();
+  result != 0
+    ? res.send(result)
+    : res.send("Impossible de charger les restaurants");
+});
+
+/***************************************** GET SPECIFIC ***************************************************** */
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await db.getRestaurant(id);
+  result != 0
+    ? res.send(result)
+    : res.send("Impossible de charger ce restaurant");
+});
+
+/***************************************** DELETE  ***************************************************** */
+
+router.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await db.deleteRestaurant(id);
+  result != 0
+    ? res.send(result)
+    : res.send("Impossible de supprimer ce restaurant");
+});
+
+export default router;
