113e89ce1fd293f376cdcb9a4c9c9745e32e9ebc
Merebe <merebe2012@gmail.com>
2023-11-01 00:49:35 +0100

    Adding a db function to verify if an email is a valid email of an user that has role = 1(worker)

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/user.js";
+import staffRoutes from "./routes/staff.js";
 import cors from "cors";
 const app = express();
 const port = 8081;
@@ -7,6 +8,7 @@ const port = 8081;
 app.use(express.json());
 app.use(cors());
 app.use("/user", userRoutes);
+app.use("/staff", staffRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
